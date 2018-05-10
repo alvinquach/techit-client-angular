d5ee8d193a56fe912db5ded403c48a9a80f64054
Unknown <alvinquach91@gmail.com>
2018-05-09 18:53:57 -0700

    added user and unit models

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,14 @@
+import { Unit } from "./unit.model";
+
+export interface User {
+    _id?: string;
+    department?: string;
+    email?: string;
+    phoneNumber?: string;
+    enabled: boolean;
+    firstName: string;
+    lastName: string;
+    position: string;
+    username: string;
+    unit: Unit;
+}
