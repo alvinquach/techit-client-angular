77cb17477325fc5bdb9f01a590012156c3952de5
Unknown <alvinquach91@gmail.com>
2018-05-10 12:59:23 -0700

    added getTicketsByUser method in users data service

--- src/app/services/data/user-data.service.ts
+++ src/app/services/data/user-data.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from "../../../environments/environment";
 import { User } from "../../models/user.model";
+import { Ticket } from "../../models/ticket.model";
 
 
 @Injectable()
@@ -14,6 +15,10 @@ export class UserDataService {
 
     getUser(userId: string): Observable<User> {
         return this._http.get<User>(`${environment.serverUrl}${this._apiPath}/${userId}`);
+    }
+
+    getTicketsByUser(userId: string): Observable<Ticket[]> {
+        return this._http.get<Ticket[]>(`${environment.serverUrl}${this._apiPath}/${userId}/tickets`);
     }
 
 }
